47dae251834d4a73c128e15976aac288a012569d
mingu <alsrn7590@naver.com>
2022-07-07 02:20:45 +0900

    SHA256.java 추가
    암호화 클래스 추가

---
+++ src/encryption/SHA256.java
@@ -0,0 +1,24 @@
+package encryption;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+    public static String encryptSHA256(String str) {
+        String sha = "";
+        try {
+            MessageDigest sh = MessageDigest.getInstance("허허허");
+            sh.update(str.getBytes());
+            byte byteData[] = sh.digest();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            sha = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Encrypt Error - NoSuchAlgorithmException");
+            sha = null;
+        }
+        return sha;
+    }
+}
